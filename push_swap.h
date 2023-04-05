84725ab6b469a8928b8abf30066f7d4af09f98fc
kaoutar-12 <kmouradi@student.1337.ma>
2023-04-05 01:22:40 +0000

    l

---
+++ push_swap.h
@@ -0,0 +1,14 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <ctype.h>
+# include <string.h>
+
+int check_if_only_dig(char **av);
+char **ft_split(char const *s, char c);
+int ft_atoi(const char *str);
+int check_if_only_dig(char **av);
+#endif
