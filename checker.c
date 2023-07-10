a2408802952898d894597b34f200e851d86d24a9
maixin-k <maixin095@gmail.com>
2023-07-10 18:01:27 +0100

    checker

--- checker.c
+++ checker.c
@@ -6,7 +6,7 @@
 /*   By: kmouradi <kmouradi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 17:23:24 by kmouradi          #+#    #+#             */
-/*   Updated: 2023/07/10 17:46:17 by kmouradi         ###   ########.fr       */
+/*   Updated: 2023/07/10 18:01:11 by kmouradi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,24 @@ int	ft_checker(t_stack *stack)
 
 void	check_move(t_stack *stack_a, t_stack *stack_b, char *move)
 {
-	if ()
+	if (ft_strcmp(move, "sa\n") == 0)
+		ft_sa(stack_a);
+	else if (ft_strcmp(move, "sb\n") == 0)
+		ft_sb(stack_b);
+	else if (ft_strcmp(move, "ra\n") == 0)
+		ft_ra(stack_a);
+	else if (ft_strcmp(move, "rb\n") == 0)
+		ft_rb(stack_b);
+	else if (ft_strcmp(move, "rra\n") == 0)
+		ft_rra(stack_a);
+	else if (ft_strcmp(move, "rrb\n") == 0)
+		ft_rrb(stack_b);
+	else if (ft_strcmp(move, "pa\n") == 0)
+		ft_pa(stack_b, stack_a);
+	else if (ft_strcmp(move, "pb\n") == 0)
+		ft_pb(stack_a, stack_b);
+	else
+		print_error();
 }
 
 int	main(int ac, char **av)
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: kmouradi <kmouradi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/08 14:41:26 by kmouradi          #+#    #+#             */
-/*   Updated: 2023/07/10 14:26:42 by kmouradi         ###   ########.fr       */
+/*   Updated: 2023/07/10 18:00:38 by kmouradi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,5 +69,6 @@ void	ft_sort2(t_stack *stack_a);
 void	print_error(void);
 int		ft_check_intmax(char *arr);
 void	ft_swap_index(int *a, int *b);
+void	print_error(void);
 
 #endif
